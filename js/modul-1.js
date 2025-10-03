840e6a7b1ee799b844150bb365d9242b5e621250
Dvizhok <kalibry1@gmail.com>
2025-10-03 12:40:38 +0300

    1

--- js/modul-1.js
+++ js/modul-1.js
@@ -1,1 +1,50 @@
-console.log(10 + 60 + 30);
+// const username = 'Dmitriy';
+// const message = `Username ${username} is ${username.length} characters long`;
+// console.log(message);
+
+// const product = 'Repair droфаіафвфвпф';
+// console.log(product[product.length - 1]);
+
+// console.log(Number.parseInt('5df'));
+// console.log(Math.random());
+
+// console.log((0.1 + 0.2).toFixed(6) === (0.3).toFixed(6));
+// function makeMessage(username) {
+//   console.log(`Hello ${username}`);
+// }
+
+// const cviz = makeMessage('Jacob');
+// console.log(cviz);
+
+// const value = "I'm a global variable";
+// function foo() {
+//   console.log(value);
+// }
+// foo();
+// console.log(value);
+
+//задача №1
+function makeTransaction(quantity, pricePerDroid) {
+  return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
+}
+console.log(makeTransaction(5, 3000));
+console.log(makeTransaction(3, 1000));
+console.log(makeTransaction(10, 500));
+
+//задача №2
+function getShippingMessage(country, price, deliveryFee) {
+  return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
+}
+console.log(getShippingMessage('Australia', 120, 50));
+console.log(getShippingMessage('Germany', 80, 20));
+console.log(getShippingMessage('Sweden', 100, 20));
+
+//задача №3
+function getElementWidth(content, padding, border) {
+  return (
+    Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2
+  );
+}
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
